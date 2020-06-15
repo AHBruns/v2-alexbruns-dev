75a930f00db47868d0de8bc2a46f85985e981801
Alex Bruns <ahb@Alexs-MacBook-Pro.local>
2020-06-15 14:50:43 -0500

    AHB :: fixes

--- pages/books.tsx
+++ pages/books.tsx
@@ -3,37 +3,24 @@ import PageBorder from "../components/PageBorder";
 import Link from "next/link";
 
 function Books({}) {
-  const books = [
-    {
-      timestamp: 1592204543000,
-      title: "Harry Potter and The Sorcerer's Stone",
-      url: "/books/harry-potter-and-the-sorcerer's-stone",
-    },
-  ];
-
   return (
     <PageBorder>
       <Title>Books</Title>
-      {books?.length ? (
-        <ul className="pl-6 list-disc">
-          {books.map(({ title, timestamp, url }) => (
-            <Link href={url}>
-              <a>
-                <li className="tracking-wider text-gray-700 text-lighter">
-                  {title}{" "}
-                  <span className="text-xs text-gray-400">
-                    {new Date(timestamp).toDateString()}
-                  </span>
-                </li>
-              </a>
-            </Link>
-          ))}
-        </ul>
-      ) : (
-        <p className="tracking-wider text-gray-700 text-lighter">
-          Nothing here yet. Check back soon, I guess.
-        </p>
-      )}
+      <ul className="pl-6 list-disc">
+        <Link href="/books/harry-potter-and-the-sorcerer's-stone">
+          <a>
+            <li className="tracking-wider text-gray-700 text-lighter">
+              {"Harry Potter and The Sorcerer's Stone"}{" "}
+              <span className="text-xs text-gray-400">
+                {new Date(1592204543000).toDateString()}
+              </span>
+            </li>
+          </a>
+        </Link>
+      </ul>
+      <p className="tracking-wider text-gray-700 text-lighter">
+        Nothing here yet. Check back soon, I guess.
+      </p>
     </PageBorder>
   );
 }
