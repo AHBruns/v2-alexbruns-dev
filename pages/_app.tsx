00aaceacd9e12ab102e53fd8bc77b10e0814f565
Alex Bruns <ahb@Alexs-MacBook-Pro.local>
2020-07-29 18:44:33 -0500

    AHB :: title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,9 @@ import Header from "../components/Header";
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <head>
+        <title>Alex Bruns</title>
+      </head>
       <div className="relative w-full h-screen px-4 pt-12 pb-4 max-w-screen">
         <Component {...pageProps} />
       </div>
