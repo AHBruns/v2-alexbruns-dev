bee7d254f94923c3e6e94531d556591183aebf54
Alex Bruns <ahb@Alexs-MacBook-Pro.local>
2020-07-29 17:50:11 -0500

    AHB :: the lightning thief

--- pages/books.tsx
+++ pages/books.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from "next";
+import { useState } from "react";
 
 type Genre = "Fantasy" | "Young Adult" | "Nonfiction" | "History" | "Business";
 
@@ -10,52 +11,94 @@ function Books({
   books: {
     title: string;
     author: string;
+    series?: string;
     startDate: string;
     endDate?: string;
     genres: Genre[];
   }[];
 }) {
+  const [filters, setFilters] = useState({ series: undefined } as any);
   return (
     <>
-      <ul className="py-4 space-y-4">
-        {books.map((book, i) => {
-          return (
-            <li
-              key={i}
-              className="p-2 tracking-wider text-gray-700 bg-gray-100 border border-gray-300 rounded-lg"
-            >
-              <p className="text-xs text-gray-500">
-                {new Date(book.startDate).toDateString()}
-                {" - "}
-                {book.endDate
-                  ? new Date(book.endDate).toDateString()
-                  : "In Progress"}
-              </p>
-              <p className="space-x-2">
-                {book.genres.map((genre, i) => (
-                  <span
-                    key={i}
-                    className="px-2 py-1 text-xs text-white bg-gray-700 rounded-md shadow-sm"
+      {filters.series && (
+        <button
+          className="fixed z-20 px-4 py-2 text-white bg-gray-800 rounded-full hover:bg-gray-700 focus:outline-none focus:shadow-outline left-4 bottom-4"
+          onClick={() => setFilters({})}
+        >
+          Reset Filters
+        </button>
+      )}
+      <div className="flex flex-col">
+        <ul className="flex-1 py-4 space-y-4 ">
+          {books
+            .filter((book) => {
+              return (
+                filters.series === undefined || book.series === filters.series
+              );
+            })
+            .map((book, i) => {
+              const bookElem = (
+                <li
+                  key={i}
+                  className="relative z-10 p-2 overflow-hidden tracking-wider text-gray-700 bg-gray-100 border border-gray-300 rounded-md shadow-lg"
+                >
+                  <p className="text-xs text-gray-500">
+                    {new Date(book.startDate).toDateString()}
+                    {" - "}
+                    {book.endDate
+                      ? new Date(book.endDate).toDateString()
+                      : "In Progress"}
+                  </p>
+                  <p className="space-x-2">
+                    {book.genres.map((genre, i) => (
+                      <span
+                        key={i}
+                        className="px-2 py-1 text-xs text-white bg-gray-700 rounded-md shadow-sm"
+                      >
+                        {genre}
+                      </span>
+                    ))}
+                  </p>
+                  <p className="text-lg">{book.title}</p>
+                  <p className="text-sm text-gray-500">by {book.author}</p>
+                </li>
+              );
+              const wrappedBookElem = book.series ? (
+                <div className="p-3 space-y-3 bg-gray-100 border border-gray-300 rounded-lg shadow-lg">
+                  <p
+                    onClick={() =>
+                      setFilters((prev) => ({ ...prev, series: book.series }))
+                    }
+                    className="z-0 p-2 mr-auto tracking-wider text-gray-100 bg-gray-900 border border-gray-700 rounded-md shadow-lg cursor-pointer magic-hover"
                   >
-                    {genre}
-                  </span>
-                ))}
-              </p>
-              <p className="text-lg">{book.title}</p>
-              <p className="text-sm text-gray-500">by {book.author}</p>
-            </li>
-          );
-        })}
+                    {book.series}
+                  </p>
+                  {bookElem}
+                </div>
+              ) : (
+                bookElem
+              );
+              return <div className="magic-hover">{wrappedBookElem}</div>;
+            })}
+        </ul>
         <p className="text-xs text-center text-gray-500">
           Last updated: {new Date(lastUpdated).toDateString()}
         </p>
-      </ul>
-      <div className="max-w-md mx-auto border-b border-gray-100" />
-      <p className="max-w-md py-4 mx-auto text-xs text-center text-gray-500">
-        If you want to give me a book recommendation or want to read one of
-        these books but can't find it anywhere, shoot me an email at&nbsp;
-        <strong>alex@alexbruns.dev</strong>.
-      </p>
+        <div className="self-center w-full max-w-md py-2 border-b border-gray-100 " />
+        <p className="max-w-md py-4 mx-auto text-xs text-center text-gray-500">
+          If you want to give me a book recommendation or want to read one of
+          these books but can't find it anywhere, shoot me an email at&nbsp;
+          <strong>alex@alexbruns.dev</strong>.
+        </p>
+      </div>
+      <style jsx>{`
+        .magic-hover {
+          transition: 300ms ease-in-out;
+        }
+        .magic-hover:hover {
+          transform: scale(1.005);
+        }
+      `}</style>
     </>
   );
 }
@@ -64,6 +107,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const books: {
     title: string;
     author: string;
+    series?: string;
     startDate: string;
     endDate?: string;
     genres: Genre[];
@@ -71,6 +115,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
     {
       title: "Harry Potter and the Sorcerer's Stone",
       author: "J.K. Rowling",
+      series: "Harry Potter",
       startDate: "17 June 2020",
       endDate: "24 Jun 2020",
       genres: ["Fantasy", "Young Adult"],
@@ -78,6 +123,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
     {
       title: "Harry Potter and the Chamber of Secrets",
       author: "J.K. Rowling",
+      series: "Harry Potter",
       startDate: "24 Jun 2020",
       endDate: "28 Jun 2020",
       genres: ["Fantasy", "Young Adult"],
@@ -85,6 +131,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
     {
       title: "Harry Potter and the Prisoner of Azkaban",
       author: "J.K. Rowling",
+      series: "Harry Potter",
       startDate: "28 Jun 2020",
       endDate: "1 Jul 2020",
       genres: ["Fantasy", "Young Adult"],
@@ -92,6 +139,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
     {
       title: "Harry Potter and the Goblet of Fire",
       author: "J.K. Rowling",
+      series: "Harry Potter",
       startDate: "1 Jul 2020",
       endDate: "7 Jul 2020",
       genres: ["Fantasy", "Young Adult"],
@@ -99,6 +147,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
     {
       title: "Harry Potter and the Order of the Phoenix",
       author: "J.K. Rowling",
+      series: "Harry Potter",
       startDate: "7 Jul 2020",
       endDate: "11 Jul 2020",
       genres: ["Fantasy", "Young Adult"],
@@ -106,6 +155,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
     {
       title: "Harry Potter and the Half-Blood Prince",
       author: "J.K. Rowling",
+      series: "Harry Potter",
       startDate: "11 Jul 2020",
       endDate: "13 Jul 2020",
       genres: ["Fantasy", "Young Adult"],
@@ -113,6 +163,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
     {
       title: "Harry Potter and the Deathly Hallows",
       author: "J.K. Rowling",
+      series: "Harry Potter",
       startDate: "13 Jul 2020",
       endDate: "17 Jul 2020",
       genres: ["Fantasy", "Young Adult"],
@@ -135,6 +186,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
     },
     {
       title: "The Lightning Thief",
+      series: "Percy Jackson & the Olympians",
       author: "Rick Riordan",
       startDate: "27 Jul 2020",
       genres: ["Fantasy", "Young Adult"],
