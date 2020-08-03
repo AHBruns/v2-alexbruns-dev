70a3efcbda0eb8d996296288be2640dcb007b4f4
Alex Bruns <ahb@Alexs-MacBook-Pro.local>
2020-08-03 11:49:03 -0500

    AHB :: google analytics

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class CustomDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-174360169-1"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-174360169-1');`,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
